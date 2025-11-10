f5991fa1d2ea6a6f4ebbaac45f0bab2a56a0ab27
Valentin MORIN <vmorin@lyreco.com>
2025-11-10 13:25:02 +0100

    First commit of all time

--- src/main/java/fr/vat/amapg/amapg/AmapgApplication.java
+++ src/main/java/fr/vat/amapg/amapg/AmapgApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AmapgApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmapgApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AmapgApplication.class, args);
+    }
 
 }
